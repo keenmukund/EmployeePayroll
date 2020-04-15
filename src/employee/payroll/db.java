e0301323ca14caa21577690a956a313e78dd1b6f
keenmukund <mukundzplace@gmail.com>
2020-04-15 19:48:14 +0530

    Initial Commit

---
+++ src/employee/payroll/db.java
@@ -0,0 +1,37 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee.payroll;
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author mukund
+ */
+public class db {
+    
+   
+    Connection conn = null;
+    public static Connection java_db(){
+        try{
+            
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:/home/mukund/NetBeansProjects/Employee Payroll/mydatabase");       
+            return conn;
+        }   
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+    
+  
+    
+    
+}
+    
+  
